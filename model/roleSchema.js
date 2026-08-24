7e3a089f4b634b06fbc9bd18268c2ab42a00b28a
abduraxmon-dev <coderakhimov@gmail.com>
2026-08-24 11:56:38 +0500

    first commit

--- model/roleSchema.js
+++ model/roleSchema.js
@@ -3,3 +3,7 @@ const { Schema, model } = require("mongoose");
 const roleSchema = new Schema({
   role: { type: String, enum: ["student", "teacher", "admin"] },
 });
+
+const Role = model("role", roleSchema);
+
+module.exports = { Role };
